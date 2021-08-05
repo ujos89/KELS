f9c98a36e5f336366aa0391e98b7c71eee8a589d
ujos89 <48623147+ujos89@users.noreply.github.com>
2021-08-05 19:56:25 +0900

    Add files via upload

--- PCA.py
+++ PCA.py
@@ -2,10 +2,14 @@ import numpy as np
 import pandas as pd
 import os
 
-from utils import get_csvs
+from utils import get_csvs, preprocessing_stu
 
 path = './dataset'
 path_data2013 = os.path.join(path, '2013data')
 
 raw_csvs = get_csvs()
-print(len(raw_csvs))
+
+L2Y1S = raw_csvs[0][0]
+# L2Y1P = raw_csvs[0][1]
+
+df_input, df_label = preprocessing_stu(L2Y1S)
--- utils.py
+++ utils.py
@@ -2,44 +2,112 @@ import numpy as np
 import pandas as pd
 import os
 
+from sklearn.preprocessing import StandardScaler
+
 def get_file_structure():
     path_dataset = './dataset'
     path_data2013 = os.path.join(path_dataset, '2013data')
-
-    folders = os.listdir(path_dataset)
+    
     data2013 = sorted(os.listdir(path_data2013))
+    for folder in data2013:
+        if not folder.startswith('KELS'):
+            data2013.remove(folder)    
 
-    kels_raw = []
     kels = []
     for idx, folder in enumerate(data2013):
         csv_files = sorted(os.listdir(os.path.join(path_data2013, data2013[idx])))
+        kels.append(csv_files)
+    
+        # # extract file names
+        # file_names = []
+        # for file_ in csv_files:
+        #     file_names.append(file_.split(' ')[-1].split('_')[0])
         
-        # extract file names
-        file_names = []
-        for file_ in csv_files:
-            file_names.append(file_.split(' ')[-1].split('_')[0])
-        
-        kels_raw.append(csv_files)
-        kels.append(file_names)
-
     # kels: [[L2Y1S, ...]]
-    return kels_raw, kels
+    return kels
 
 def get_csvs():
     path_dataset = './dataset'
     path_data2013 = os.path.join(path_dataset, '2013data')
     data2013 = sorted(os.listdir(path_data2013))
+    for folder in data2013:
+        if not folder.startswith('KELS'):
+            data2013.remove(folder)  
 
-    kels_raw, kels = get_file_structure()
+    kels = get_file_structure()
     csv_files = []
 
-    for kels_idx, folder in enumerate(kels_raw):
+    for kels_idx, folder in enumerate(kels):
         csv_year = []
         
         for idx, survey in enumerate(folder):
-            exec("%s = pd.read_csv(os.path.join(path_data2013, data2013[kels_idx], survey))" % (kels[kels_idx][idx]))
-            exec("csv_year.append(%s)" % (kels[kels_idx][idx]))
+            if survey.endswith('.csv'):
+                csv_year.append(pd.read_csv(os.path.join(path_data2013, data2013[kels_idx], survey), low_memory=False, thousands=','))
+            # exec("%s = pd.read_csv(os.path.join(path_data2013, data2013[kels_idx], survey), low_memory=False, thousands=',')" % (kels[kels_idx][idx]))
+            # exec("csv_year.append(%s)" % (kels[kels_idx][idx]))
         
         csv_files.append(csv_year)
 
     return csv_files
+
+def preprocessing_stu(dataframe):
+    
+    columns = list(dataframe.columns)
+    columns_del = []
+    columns_basicinfo = ['L2GENDER', 'L2Y1_SCHID', 'L2Y1_REG', 'L2Y1_SCHSIZE', 'L2Y1_SCHTYPE', 'L2Y1S']
+    columns_categorical = ['L2Y1S35', 'L2Y1S34', 'L2Y1S33']
+    columns_0to9 =['L2Y1S28', 'L2Y1S2901', 'L2Y1S2902', 'L2Y1S31']
+    columns_0or1 = ['L2Y1S22', 'L2Y1S23', 'L2Y1S24', 'L2Y1S25']
+    columns_1to4 = ['L2Y1S1001', 'L2Y1S1002', 'L2Y1S1003']
+    # lots of missing value in question about addicting on phone
+    columns_phone = ['L2Y1S3001', 'L2Y1S3002', 'L2Y1S3003', 'L2Y1S3004', 'L2Y1S3005']
+
+    for col in columns:
+        if col.endswith('O'):
+            columns_del.append(col)
+
+        if col.startswith('L2Y1S24_'):
+            columns_del.append(col)
+
+    columns_del.extend(columns_basicinfo)
+    columns_del.extend(columns_categorical)
+    columns_del.extend(columns_phone)
+
+    # delete unnecessary columns
+    df_drop = dataframe.drop(columns_del, axis=1)
+
+    # delete null value
+    df_drop = df_drop[df_drop != "#NULL!"]
+    # print(df_drop.shape)
+
+    # string to int (dataframe)
+    df_float = df_drop.astype(float)
+    # L2SID float to int
+    df_float['L2SID'] = df_float['L2SID'].apply(np.int64)
+
+    # drop nan and invalid value
+    df_prepared = df_float.dropna(axis=0) 
+    df_prepared = df_prepared[df_prepared >= 0]
+    df_prepared = df_prepared.dropna(axis=0) 
+    df_prepared = df_prepared.set_index(['L2SID'])
+    
+    # split dataframe to target or not 
+    columns_target = ['L2Y1_K_SCORE', 'L2Y1_E_SCORE', 'L2Y1_M_SCORE', 'L2Y1_K_CS', 'L2Y1_E_CS', 'L2Y1_M_CS', 'L2Y1_K_THETA', 'L2Y1_E_THETA','L2Y1_M_THETA']
+    df_target = df_prepared[columns_target]
+    df_prepared = df_prepared.drop(columns=columns_target)
+
+    # data preprocessing
+    columns_prepared = list(df_prepared.columns)
+
+    # move value(has another sclae) to fit average
+    for col in columns_prepared:
+        if col in columns_0to9:
+            df_prepared[col] -= 1.5
+        elif col in columns_1to4:
+            df_prepared[col] += .5
+
+    # standard scaling for each row (averaging students intends)
+    scaler=StandardScaler()
+    df_scaled = pd.DataFrame(scaler.fit_transform(df_prepared.T).T, columns = df_prepared.columns)
+
+    return df_scaled, df_target
