ad20e32a5cb51dcde6e200a81f1b5755e19933a7
ujos89 <rkd89.ml@gmail.com>
2021-11-12 02:08:42 +0900

    dataset kels

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-import numpy as np
 import pandas as pd
 import os
 from utils.utils import *
-from utils.mergedata import *
 
-merge_preprocessed_outerjoin()
+# check dataset verify
+root_dir = './preprocessed/merge/outer'
+input_df = pd.read_csv(os.path.join(root_dir, 'input_merge.csv')).set_index('L2SID')
+label_df = pd.read_csv(os.path.join(root_dir, 'label_merge.csv')).set_index('L2SID')
+
+print(input_df.iloc[0])
+print(input_df.iloc[0].isna())
+print(label_df.iloc[0])
+print(label_df.iloc[0].isna())
+print()
+
+print(label_df.iloc[0].index[label_df.iloc[0].isna()].tolist())
+
+def get_year(series):
+    nan_index = series.index[series.notna()].tolist()
+    year = sorted(list(set([int(_[3]) for _ in nan_index])))
+    year_col = ['L2Y'+str(y) for y in year]
+    
+    return year_col
+
+series = label_df.iloc[0]
+
+print(get_year(series))
--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,18 +1,36 @@
 import torch
 import os
+import pandas as pd
 from torch.utils.data import Dataset
+
+def get_year(series):
+    nan_index = series.index[series.notna()].tolist()
+    year = sorted(list(set([int(_[3]) for _ in nan_index])))
+    year_col = ['L2Y'+str(y) for y in year]
+    
+    return year, year_col
+    
 
 class KELS(Dataset):
     def __init__(self, root_dir='./preprocessed/merge/outer'):
         self.root_dir = root_dir
-    
+        self.input_df = pd.read_csv(os.path.join(self.root_dir, 'input_merge.csv')).set_index('L2SID')
+        self.label_df = pd.read_csv(os.path.join(self.root_dir, 'label_merge.csv')).set_index('L2SID')
+   
     def __len__(self):
-        return len()
+        if len(self.label_df) == len(self.input_df):
+            return len(self.label_df)
     
-    def __getitem__(self,idx):
+        return False
+    
+    def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
             
+        year, year_col = get_year(self.label_df.iloc[idx])
+        
+        
+        
         
         
         
